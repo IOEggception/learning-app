6db690c8208adc27c0d405850a088d1bba8f9c74
Sean Lacey <sean.lacey@libertymutual.com>
2023-03-10 12:43:47 -0500

    adding logic to parse json/html style added models

---
+++ LearningApp/Models/Module.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  LearningApp
+//
+//  Created by Sean.Lacey on 3/10/23.
+//
+
+import Foundation
+
+struct Module : Decodable, Identifiable {
+    
+    var id: Int
+    
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson : Decodable, Identifiable {
+    var id:Int
+    var title:String
+    var video:String
+    var duration:String
+    var explanation:String
+}
+
+struct Test: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,5 +9,40 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init() {
+        
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let decoder = JSONDecoder()
+            
+            do{
+                let modules = try decoder.decode([Module].self, from: jsonData)
+                self.modules = modules
+            }
+            catch {
+                print("Error decoding into module array")
+            }
+        }
+        catch {
+            print("Error turning url to data")
+        }
+        
+        // Parse style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            self.styleData = try Data(contentsOf: styleUrl!)
+        } catch {
+            print("error converting style URL to data")
+        }
+    }
 }
